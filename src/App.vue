985d9a13003338adfd0e06310d9550c139faadf6
fuwenbin <390201580@qq.com>
2018-04-16 15:59:09 +0800

    11

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <appnav></appnav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import appnav from '@/components/nav'
+
+export default {
+  name: 'App',
+  components:{
+    appnav
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="appnav">
+		<el-button type="primary" @click="show">123</el-button>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'appnav',
+		methods:{
+			show(){
+				this.$message('hello')
+			}
+		}
+	}
+</script>
